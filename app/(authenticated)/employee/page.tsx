a80a8b13bd1c09b8c21463fc2ab778874aa51a04
devxploit <brigoliboonjefferson@gmail.com>
2026-08-26 08:42:56 +0800

    resolved build issue

--- app/(authenticated)/employee/page.tsx
+++ app/(authenticated)/employee/page.tsx
@@ -142,7 +142,7 @@ export default function EmployeeDashboardPage() {
       </motion.div>
 
       <motion.div variants={itemVariants}>
-        <WeeklyMenu data={{ weekOf: "", items: { chicken: [], beef: [], seafood: [], salad: [], wrap: [], breakfast: [], pasta: [], soup: [], pizza: [], burgers: [], drinks: [], biryani: [], risotto: [] } }} />
+        <WeeklyMenu data={{ weekOf: "", items: { chicken: [], beef: [], seafood: [], salad: [], wrap: [], breakfast: [], pasta: [], soup: [], pizza: [], burgers: [], drinks: [], biryani: [], risotto: [], smoothie: [], juice: [], beverages: [] } }} />
       </motion.div>
 
       <motion.div variants={itemVariants}>
